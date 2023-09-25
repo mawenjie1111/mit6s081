7aefee4b644bb2707b4e95292d060780c89cac54
mama <1468382678@qq.com>
2023-09-25 21:59:22 +0800

    xargs

--- user/xargs.c
+++ user/xargs.c
@@ -1,18 +1,59 @@
+// xargs.c
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-#include "kernel/param.h"
+#include "kernel/fs.h"
 
-int main(int argc, char const *argv[])
-{
+// 带参数列表，执行某个程序
+void run(char *program, char **args) {
+	if(fork() == 0) { // child exec
+		exec(program, args);
+		exit(0);
+	}
+	return; // parent return
+}
 
-    char buf[MAXARG];
-    int j=0;
-    while (read(0,buf+j,1))
-    {
-        printf("%c\n",buf[j]);
-        /* code */
-        j++;
-    }
-    exit(0);
+int main(int argc, char *argv[]){
+	char buf[2048]; // 读入时使用的内存池
+	char *p = buf, *last_p = buf; // 当前参数的结束、开始指针
+	char *argsbuf[128]; // 全部参数列表，字符串指针数组，包含 argv 传进来的参数和 stdin 读入的参数
+	char **args = argsbuf; // 指向 argsbuf 中第一个从 stdin 读入的参数
+	for(int i=1;i<argc;i++) {
+		// 将 argv 提供的参数加入到最终的参数列表中
+		*args = argv[i];
+		args++;
+	}
+	char **pa = args; // 开始读入参数
+	while(read(0, p, 1) != 0) {
+		if(*p == ' ' || *p == '\n') {
+			// 读入一个参数完成（以空格分隔，如 `echo hello world`，则 hello 和 world 各为一个参数）
+			*p = '\0';	// 将空格替换为 \0 分割开各个参数，这样可以直接使用内存池中的字符串作为参数字符串
+						// 而不用额外开辟空间
+						// 使用 /0 可以把char 数组 分割 ，然后用字符串（指针数组）读取该char数组，就可以获得以/0为分割符的字符串
+						// 妙啊！！！！！！！！！！！！！
+			*pa= last_p;//在args末位处添加参数，即从
+            pa++;
+			last_p = p+1;
+
+			if(*p == '\n') {
+				// 读入一行完成
+				*pa = 0; // 参数列表末尾用 null 标识列表结束
+				run(argv[1], argsbuf); // 执行最后一行指令
+				pa = args; // 重置读入参数指针，准备读入下一行
+						   // 重新索引到args处，进行覆盖就可以
+			}
+		}
+		p++;
+	}
+	if(pa != args) { // 如果最后一行不是空行,这是指最后的参数行不是以空格或者回车结束，执行同样的方法
+		// 收尾最后一个参数
+		*p = '\0';
+		*(pa++) = last_p;
+		// 收尾最后一行
+		*pa = 0; // 参数列表末尾用 null 标识列表结束
+		// 执行最后一行指令
+		run(argv[1], argsbuf);
+	}
+	while(wait(0) != -1) {}; // 循环等待所有子进程完成，每一次 wait(0) 等待一个
+	exit(0);
 }
